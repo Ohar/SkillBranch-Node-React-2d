07669fd1c54c1d992702a7311f686e79b6b564b7
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:40:51 +0300

    Add unifyColorFromRgb logic

--- scripts/formatters/unify-color-from-rgb.js
+++ scripts/formatters/unify-color-from-rgb.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const convert = require('color-convert');
+
 function unifyColorFromRgb (colorInput) {
-  return colorInput;
+  colorInput = colorInput.toLowerCase();
+
+  const {red, green, blue} = colorInput.match(/^\D+(\d+)\D+(\d+)\D+(\d+)\D+$/g);
+
+  return convert.rgb.hex(red, green, blue);
 }
 
 module.exports = unifyColorFromRgb;
