87ae244d412fae5af5422a71abc9a1aa3dc588c9
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:51:29 +0300

    Add unifyColorFromWord code

--- scripts/formatters/unify-color-from-word.js
+++ scripts/formatters/unify-color-from-word.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const COLOR_LIST = require('./../../const/color-dict');
+
 function unifyColorFromWord (colorInput) {
-  return colorInput;
+  colorInput = colorInput.trim().toLowerCase();
+  return COLOR_LIST[colorInput];
 }
 
 module.exports = unifyColorFromWord;
