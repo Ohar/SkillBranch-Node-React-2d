93c31bd14e3e643aede8033aa7fea7c093ac122d
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:30:49 +0300

    Add unifyColorFromHex logic

--- scripts/formatters/unify-color-from-hex.js
+++ scripts/formatters/unify-color-from-hex.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function unifyColorFromHex (colorInput) {
-  return colorInput;
+  colorInput = colorInput.toLowerCase().replace('#', '');
+
+  const isShort = colorInput.length === 3;
+
+  if (isShort) {
+    colorInput += colorInput
+  }
+
+  return `#${colorInput}`;
 }
 
 module.exports = unifyColorFromHex;
