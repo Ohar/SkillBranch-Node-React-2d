73f639559e8f5f354acef875de2a5a7a0b38173a
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 18:02:00 +0300

    Add checkRgb test

---
+++ test/checkers/check-rgb.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const chai     = require('chai'),
+      assert   = chai.assert,
+      checkRgb = require('./../../scripts/checkers/check-rgb');
+
+describe(
+  'checkRgb', () => {
+    it(
+      'Возвращает функцию', () => {
+        assert.isFunction(checkRgb);
+      }
+    );
+
+    describe(
+      'Правильно обрабатывает строку', () => {
+
+        it(
+          'rgb(255, 179, 179) → true', () => {
+            assert.isTrue(checkRgb('rgb(255, 179, 179)'));
+          }
+        );
+
+        it(
+          'hsl(0, 100%, 0%) → false', () => {
+            assert.isFalse(checkRgb('hsl(0, 100%, 0%)'));
+          }
+        );
+
+        it(
+          'fff → false', () => {
+            assert.isFalse(checkRgb('fff'));
+          }
+        );
+
+        it(
+          'red → false', () => {
+            assert.isFalse(checkRgb('red'));
+          }
+        );
+
+        it(
+          'Turquoise → false', () => {
+            assert.isFalse(checkRgb('Turquoise '));
+          }
+        );
+
+      }
+    );
+  }
+);
