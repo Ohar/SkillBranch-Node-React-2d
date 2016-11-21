38acc4b22c0f175e6ab31a6c81d4bb46a3372200
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:57:10 +0300

    checkColorWord: fix leys

--- scripts/checkers/check-color-word.js
+++ scripts/checkers/check-color-word.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const findKey = require('lodash/findKey');
-
 const COLOR_LIST = require('./../../const/color-dict');
 
 function checkColorWord (str) {
   str = str.trim().toLowerCase();
-  return Boolean(findKey(COLOR_LIST, color => color === `#${str}`));
+  return Boolean(COLOR_LIST[str]);
 }
 
 module.exports = checkColorWord;
