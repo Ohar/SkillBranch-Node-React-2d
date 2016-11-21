d9aa9ea14cd28efb9f60dd28b60f8935e1561358
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:54:40 +0300

    checkColorWord: clear input

--- scripts/checkers/check-color-word.js
+++ scripts/checkers/check-color-word.js
@@ -5,6 +5,7 @@ const findKey = require('lodash/findKey');
 const COLOR_LIST = require('./../../const/color-dict');
 
 function checkColorWord (str) {
+  str = str.trim().toLowerCase();
   return Boolean(findKey(COLOR_LIST, color => color === `#${str}`));
 }
 
