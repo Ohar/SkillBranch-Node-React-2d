05f86ac574f604adcd2742f83dff9d84b80af107
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-21 17:39:40 +0300

    Add unifyColorFromHsl logic

--- scripts/formatters/unify-color-from-hsl.js
+++ scripts/formatters/unify-color-from-hsl.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const convert = require('color-convert');
+
 function unifyColorFromHsl (colorInput) {
-  return colorInput;
+  colorInput = colorInput.toLowerCase();
+
+  const {hue, saturation, lightness} = colorInput.match(/^\D+(\d+)\D+(\d+)%\D+(\d+)%\D+$/g);
+
+  return convert.hsl.hex(hue, saturation, lightness);
 }
 
 module.exports = unifyColorFromHsl;
